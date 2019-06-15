efd4c9cadaca5608cd2749b8c7afd0a3a11f6dd0
annakrystalli <annakrystalli@googlemail.com>
2019-06-15 14:32:48 +0100

    commit hex & logo materials. Related to #1

---
+++ hex/make_hex.R
@@ -0,0 +1,13 @@
+library(magick)
+library(hexSticker)
+library(dplyr)
+
+
+sticker(here::here("assets", "reprohack_image_high.png"),
+        package="ReproHack", 
+        p_size=7, s_x=1.0, s_y=0.8, s_width=1.05, 
+        p_color = "#16a085", l_y = 1.05, l_x = 0.9, 
+        h_fill="white", h_color="#16a085", h_size = 2.5,
+        filename="hex/reprohack.png", spotlight = F, asp = 0.7679814)
+
+
