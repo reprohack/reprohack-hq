78a1dd68f59caaa604a2271074dd5af55184d258
annakrystalli <annakrystalli@googlemail.com>
2019-07-18 21:25:55 +0100

    add new (illustrator) people hex logo

--- hex/make_hex.R
+++ hex/make_hex.R
@@ -1,11 +1,29 @@
 library(hexSticker)
+library(showtext)
 
-# The reprohack_image_high.png was made using image.pptx, also in assets
-sticker(here::here("assets", "reprohack_image_high.png"),
-        package="ReproHack", 
-        p_size=7, s_x=1.0, s_y=0.8, s_width=1.05, 
-        p_color = "#16a085", l_y = 1.05, l_x = 0.9, 
-        h_fill="white", h_color="#16a085", h_size = 2.5,
-        filename="hex/reprohack.png", spotlight = F, asp = 0.7679814)
+font_add_google("Knewave", "knewave")
+showtext_auto()
+# The reprohack-graphic1.png was made in adobe illustrator (not currently in repo)
+sticker(here::here("assets", "reprohack-graphic1.png"),
+        package="ReproHack", p_family = "knewave",
+        p_size=7, p_color = "#FFFFFF", p_y = 0.6,
+        s_x=1.0, #s_y=0.8, 
+        s_width=0.7, s_y = 1.2,
+        l_y = 1.05, l_x = 0.9, 
+        h_fill="#18ba99", h_color="#10735F", 
+        #h_size = 3,
+        filename="hex/reprohack.png", spotlight = F, asp = 0.7679814,
+        dpi=2700)
 
-
+# svg output. Not currently working very well as reading in the svg version of the
+# artwork is super pixelated
+sticker(here::here("assets", "reprohack-graphic.svg"),
+        package="ReproHack", p_family = "knewave",
+        p_size=7, p_color = "#FFFFFF", p_y = 0.6,
+        s_x=1.0, #s_y=0.8, 
+        s_width=0.7, s_y = 1.2,
+        l_y = 1.05, l_x = 0.9, 
+        h_fill="#18ba99", h_color="#10735F", 
+        #h_size = 3,
+        filename="hex/reprohack.svg", spotlight = F, asp = 0.7679814,
+        dpi=2700)
