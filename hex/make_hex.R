231efe9940b03b5bc88db17e96ba568c00ebe7be
annakrystalli <annakrystalli@googlemail.com>
2019-07-24 13:52:42 +0100

    Lighten hex fill colour

--- hex/make_hex.R
+++ hex/make_hex.R
@@ -3,14 +3,17 @@ library(showtext)
 
 font_add_google("Knewave", "knewave")
 showtext_auto()
-# The reprohack-graphic1.png was made in adobe illustrator (not currently in repo)
+# The reprohack-graphic1.png was made in adobe illustrator (not currently in repo). The
+# original fill colour was #18ba99 but was: 
+# a) too dark according to feedback &
+# b) out of gamut for CMYK print so corrected
 sticker(here::here("assets", "reprohack-graphic1.png"),
         package="ReproHack", p_family = "knewave",
         p_size=7, p_color = "#FFFFFF", p_y = 0.6,
         s_x=1.0, #s_y=0.8, 
         s_width=0.7, s_y = 1.2,
         l_y = 1.05, l_x = 0.9, 
-        h_fill="#18ba99", h_color="#10735F", 
+        h_fill="#00cc99", h_color="#10735F", 
         #h_size = 3,
         filename="hex/reprohack.png", spotlight = F, asp = 0.7679814,
         dpi=2700)
@@ -27,3 +30,6 @@ sticker(here::here("assets", "reprohack-graphic.svg"),
         #h_size = 3,
         filename="hex/reprohack.svg", spotlight = F, asp = 0.7679814,
         dpi=2700)
+
+
+
