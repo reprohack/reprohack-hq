b49c20c3b427c21caac82e9494029aef1c15c3ec
annakrystalli <annakrystalli@googlemail.com>
2019-10-01 16:40:08 +0100

    add tic

---
+++ tic.R
@@ -0,0 +1,12 @@
+# TODO: Define steps using the tic DSL, see ?DSL
+# get_stage("<stage name>") %>%
+#   add_step(step_...(...))
+
+get_stage("install") %>%
+    add_code_step(install.packages("rmarkdown"))
+
+
+
+get_stage("deploy") %>%
+    add_code_step( rmarkdown::render("README.Rmd", run_pandoc = FALSE)) %>%
+    add_step(step_push_deploy())
