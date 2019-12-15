d2a6e29e3c39dd235e5a9fb640de97d948b8618e
annakrystalli <annakrystalli@googlemail.com>
2019-12-15 14:40:47 +0000

    use step_run_code instead of add_code_step

--- tic.R
+++ tic.R
@@ -3,11 +3,11 @@
 #   add_step(step_...(...))
 
 get_stage("install") %>%
-    add_code_step(install.packages(c("rmarkdown", "dplyr", 
-                                     "leaflet", "glue", "readr")))
+    add_step( step_run_code(install.packages(c("rmarkdown", "dplyr", 
+                                     "leaflet", "glue", "readr"))))
 
 
 
 get_stage("deploy") %>%
-    add_code_step( rmarkdown::render("README.Rmd", run_pandoc = FALSE)) %>%
+    add_step( step_run_code(rmarkdown::render("README.Rmd", run_pandoc = FALSE))) %>%
     add_step(step_push_deploy())
